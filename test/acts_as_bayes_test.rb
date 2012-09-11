1fa59524ae6d8b3e472ccfde0f94756f3dd40db6
Victor Pereira <vpereira@web.de>
2012-09-11 17:03:14 +0200

    on my way to make it works

--- test/acts_as_bayes_test.rb
+++ test/acts_as_bayes_test.rb
@@ -2,9 +2,16 @@ require_relative 'test_helper'
 
 class TestActsAsBayesTest < MiniTest::Unit::TestCase
   def setup
-    Foo.destroy_all
+    #Foo.destroy_all
   end
 
+  def test_common_words
+     klass = Foo.class_eval do
+       acts_as_bayes :on=>:title
+       self
+     end
+     assert klass::COMMON_WORDS.size > 0
+  end
   def test_foo
     assert Foo.new
   end
@@ -13,7 +20,7 @@ class TestActsAsBayesTest < MiniTest::Unit::TestCase
   end
   def test_defaults
     klass = Foo.class_eval do
-      acts_as_bayes
+      acts_as_bayes :on=>:title
       self
     end
     assert ! klass.nil?
@@ -22,7 +29,7 @@ class TestActsAsBayesTest < MiniTest::Unit::TestCase
   end
   def test_acts_as_bayes_with_params_I
     klass = Foo.instance_eval do
-      acts_as_bayes :threshold=>1.0
+      acts_as_bayes :threshold=>1.0,:on=>:title
       self
     end
     assert ! klass.nil?
@@ -30,7 +37,6 @@ class TestActsAsBayesTest < MiniTest::Unit::TestCase
     k = klass.new(:title=>"my super foo is sexy")
     assert ! k.threshold.nil?
     assert k.threshold, 1.0
-
     assert k.save
   end
 end
