47b4e5d4b0b8d693ed23a1c22db837067a577c8e
Victor Pereira <vpereira@web.de>
2012-09-14 15:29:06 +0200

    added tests for bayes category

---
+++ test/bayes_category_test.rb
@@ -0,0 +1,19 @@
+require_relative 'test_helper'
+
+class TestBayesCategory < MiniTest::Should::TestCase
+  setup do
+    BayesCategory.destroy_all
+    @b = BayesCategory.new(:category=>"foo")
+  end
+
+  should "not be nil" do
+    assert ! @b.nil?
+  end
+
+  should "be able to update the words" do
+    @b.words =  { "foo"=>10, "bar"=>20 }
+    @b.save
+    assert @b.word_count > 0
+    assert @b.word_count == 30
+  end
+end
