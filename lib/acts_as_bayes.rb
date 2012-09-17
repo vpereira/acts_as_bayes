db95ac100a06ef6ab2f2ba50a385f0af634948ae
Victor Pereira <vpereira@web.de>
2012-09-17 10:27:21 +0200

    empty probabilities

--- lib/acts_as_bayes.rb
+++ lib/acts_as_bayes.rb
@@ -56,8 +56,8 @@ module ActsAsBayes
     #classify method
     def classify(default = 'unknown')
       sorted = probabilities.sort {|a,b| a[1]<=>b[1]}
-      best,second_best = sorted.pop, sorted.pop rescue []
-      return default if best.empty?
+      return default if sorted.empty?
+      best,second_best = sorted.pop, sorted.pop
       return best[0] if best[1]/second_best[1] > self.class.threshold
       default
     end
